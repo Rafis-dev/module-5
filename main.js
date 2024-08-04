85ce1f7d5a48a6b61ab07ca57b9e5d20aa38bb8f
Rafis Shigapov <rafis1work@gmail.com>
2024-08-04 20:02:36 +0300

    module 5 lesson 2

---
+++ main.js
@@ -0,0 +1,7 @@
+const modalTitle = document.querySelector('.modal__title');
+const modalId = document.querySelector('.modal__id');
+const modalButton = document.querySelector('.modal__btn');
+const modalForm = document.querySelector('.modal__form');
+const discountCheckbox = document.querySelector('.form__checkbox');
+const discountInput = document.querySelector('#discount');
+const totalPrice = document.querySelector('.amount__number');
