0fd721884e808401d51f5e06bc564b8918696daa
Rafis Shigapov <rafis1work@gmail.com>
2024-08-07 13:02:18 +0300

    made createRow and renderGoods functions

--- main.js
+++ main.js
@@ -5,3 +5,99 @@ const modalForm = document.querySelector('.modal__form');
 const discountCheckbox = document.querySelector('.form__checkbox');
 const discountInput = document.querySelector('#discount');
 const totalPrice = document.querySelector('.amount__number');
+
+const goods = [
+  {
+    'id': 253842678,
+    'title': 'Смартфон Xiaomi 11T 8/128GB',
+    'price': 27000,
+    'description': 'Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.',
+    'category': 'mobile-phone',
+    'discont': false,
+    'count': 3,
+    'units': 'шт',
+    'images': {
+      'small': 'img/smrtxiaomi11t-m.jpg',
+      'big': 'img/smrtxiaomi11t-b.jpg',
+    },
+  },
+  {
+    'id': 296378448,
+    'title': 'Радиоуправляемый автомобиль Cheetan',
+    'price': 4000,
+    'description': 'Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет',
+    'category': 'toys',
+    'discont': 5,
+    'count': 1,
+    'units': 'шт',
+    'images': {
+      'small': 'img/cheetancar-m.jpg',
+      'big': 'img/cheetancar-b.jpg',
+    },
+  },
+  {
+    'id': 215796548,
+    'title': 'ТВ приставка MECOOL KI',
+    'price': 12400,
+    'description': 'Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D',
+    'category': 'tv-box',
+    'discont': 15,
+    'count': 4,
+    'units': 'шт',
+    'images': {
+      'small': 'img/tvboxmecool-m.jpg',
+      'big': 'img/tvboxmecool-b.jpg',
+    },
+  },
+  {
+    'id': 246258248,
+    'title': 'Витая пара PROConnect 01-0043-3-25',
+    'price': 22,
+    'description': 'Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.',
+    'category': 'cables',
+    'discont': false,
+    'count': 420,
+    'units': 'v',
+    'images': {
+      'small': 'img/lan_proconnect43-3-25.jpg',
+      'big': 'img/lan_proconnect43-3-25-b.jpg',
+    },
+  },
+];
+
+const createRow = item => {
+  const row = document.createElement('tr');
+  row.classList.add('table__body-row');
+
+  row.innerHTML = `
+    <td class="table__body-data">${item.id}</td>
+    <td class="table__body-data">${item.title}</td>
+    <td class="table__body-data">${item.category}</td>
+    <td class="table__body-data table__body-data_type_unit table__body-data_text_center">${item.units}</td>
+    <td class="table__body-data table__body-data_text_center">${item.count}</td>
+    <td class="table__body-data table__body-data_text_center">${'$' + item.price
+    }</td>
+    <td class="table__body-data table__body-data_text_center">${'$' + item.price * item.count}</td>
+    <td class="table__body-data table__body-buttons table__body-data_text_right">
+      <button class="table__button" type="button">
+        <img class="table__button-img" src="assets/images/icons/image.svg" alt="иконка изображения">
+      </button>
+      <button class="table__button" type="button">
+        <img class="table__button-img" src="assets/images/icons/edit.svg" alt="иконка редактирования">
+      </button>
+      <button class="table__button" type="button">
+        <img class="table__button-img" src="assets/images/icons/delete.svg" alt="иконка корзины">
+      </button>
+    </td>
+`;
+  return row;
+};
+
+const renderGoods = items => {
+  const tbody = document.querySelector('.table__body');
+
+
+  items.map(createRow).forEach(row => tbody.append(row));
+};
+
+renderGoods(goods);
