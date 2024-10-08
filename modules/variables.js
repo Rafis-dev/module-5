a8f6a812bdae3854ebd223eba0c83e4c0af2f825
Rafis Shigapov <rafis1work@gmail.com>
2024-10-08 16:09:27 +0300

    prepare for lesson 9

--- modules/variables.js
+++
@@ -1,4 +0,0 @@
-export const totalPrice = document.querySelector('.amount__number');
-export const modalOpen = document.querySelector('.cms__header-modal');
-export const tbody = document.querySelector('.table__body');
-
